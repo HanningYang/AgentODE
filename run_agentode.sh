7c34a84289829458bf14ed422f5920add61d0bba
Hanning Yang <hanning.yang@uniklinik-freiburg.de>
2026-05-04 09:20:43 +0200

    Ignore run scripts

--- run_agentode.sh
+++
@@ -1,4 +0,0 @@
-################ AGENTODE with LOCAL LLM ################
-
-# aki
-# python main.py --problem_name aki --spec_path specs_skeleton/specification_aki_numpy.txt --log_path logs/aki_run1
